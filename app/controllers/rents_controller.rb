65bf24fa67775881708697645a231589cdae75d3
PachecoCesar <c86pacheco@gmail.com>
2019-10-11 16:57:19 -0300

    changes in rent show

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -5,11 +5,23 @@ class RentsController < ApplicationController
 
   def new
     @rent = Rent.new
-    @rent.save
-    redirect_to rent_path
+
+  end
+
+  def create
+    @rente = Rent.new(rent_params)
+    @rente.user = current_user
+    @rente.save
+    redirect_to rent_path(@rente)
   end
 
   def show
     
   end
 end
+
+
+private
+def rent_params
+      params.require(:rent).permit(:photos, :title, :description, :address, :town, :city, :postal_code, :rooms, :bath, :price, :gender, :time, :lgtb, :pets, :smoker)
+    end
