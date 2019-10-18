bd17b7cb0acfe9d4b7ab1aa22cd25e619aee3573
PachecoCesar <c86pacheco@gmail.com>
2019-10-18 14:29:04 -0300

    Show Rent done and added some style

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,35 +1,30 @@
 class RentsController < ApplicationController
+  before_action :set_rent, only: [:show, :edit, :update, :destroy]
   def index
     @rents = Rent.all
+    @rent = Rent.new
   end
 
   def new
     @rent = Rent.new
-    if @rent.save
-      redirect_to rent_path, notice: 'WENA CTM'
-    end
   end
 
   def create
-    #@rente = Rent.create(rent_params)
-    #@rente.user = current_user
-    #if @rente.save
-    #redirect_to rent_path(@rente)
-    #end
-    #@rente.user = current_user
-    @rente = Rent.new(rent_params)
-    @rente.user_id = params[:user_id]
-    if @rente.save
-      redirect_to rent_path, notice: 'WENA CTM'
-    end
+    @rent = Rent.new(rent_params)
+    @rent.user = current_user
+    @rent.save
+    redirect_to rent_path(@rent)
   end
 
   def show
-
+      @user = current_user
   end
 
   private
+  def set_rent
+     @rent = Rent.find(params[:id])
+   end
   def rent_params
-    params.require(:rent).permit(:user_id, :photos, :title, :description, :address, :town, :city, :postal_code, :rooms, :bath, :price, :gender, :time, :lgtb, :pets, :smoker)
+    params.require(:rent).permit(:user_id, :address, :town, :city, :title, :photos, :price, :description, :kind_of, :rooms, :period, :bath,)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :rent
+  has_many :rent, dependent: :destroy
 end
